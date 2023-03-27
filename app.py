b007c8db0d6056d496001f0868c0e48e01a11379
Aditya-0911 <128395061+Aditya-0911@users.noreply.github.com>
2023-03-27 10:04:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,29 @@ def calculate():
     bmi = ''
     children = ''
     smoking = ''
+    gender = ''
+    gender_female = ''
+    gender_male= ''
     insurance = ''
+
 
     if request.method == 'POST' and 'age' in request.form and 'bmi' in request.form:
         age = request.form.get('age')
         bmi = request.form.get('bmi')
         children = request.form.get('children')
         smoking = request.form.get('smoking')
+        gender = request.form.get('gender')
+        if(gender=='male'): 
+            gender_male=1
+            gender_female=0
+        elif(gender=='female'): 
+            gender_male=0
+            gender_female=1
+        # elif(smoking=='no'): smoking=0
 
-        insurance = round(8167.9  + (3194.18 * float(age)) + (1086.33 * float(bmi)) + (311.49 * int(children)) + (25050 * int(smoking)),2)
+        insurance = round(5633.51+(3715.28*int(age))+(1999.61*int(bmi))+(651.17*int(children))+(23530*int(smoking)+2616.04*int(gender_male)+3017.46*int(gender_female)),2)
 
-    return render_template("index.html", age=age, bmi=bmi, children=children, smoking=smoking, insurance=insurance)
+    return render_template("index.html", age=age, bmi=bmi, children=children, smoking=smoking,gender=gender,insurance=insurance)
 
 if __name__ == '__main__':
     app.run(debug=True)
